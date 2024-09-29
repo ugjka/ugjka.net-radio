41f1a5b6a931160803b7e840db60823cc3096693
ugjka <ugis@germanis.lv>
2024-09-29 14:19:01 +0300

    wireguard

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 find _site \( -name '*.css' -o -name '*.html' -o -name '*.js' -o -name '*.json' -o -name '*.txt' -o -name '*.xml' -o -name '*.yml' \) -exec gzip --best --keep -f {} \;
-rsync -Pvhr --delete _site/ ugjka.net:share/radio/
+rsync -Pvhr --delete _site/ server.internal:share/radio/
